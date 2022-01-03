7e75fd6dd1910aacb5d30b36dbf21fec209fa86e
ekerintaiwo <ekerinolufemo>
2022-01-03 16:49:58 +0100

    fifth comit

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -28,6 +28,15 @@ class Clock extends Component {
 
       }
 
+
+       leadingzero(num){
+
+          return  num < 10 ? '0' + num : num  ;  
+       }
+
+
+
+
       componentDidUpdate() {
 
           setInterval( ()=>this.getTimeUntil(this.props.deadline),1000) ;
@@ -41,7 +50,7 @@ class Clock extends Component {
      getTimeUntil(deadline){
 
        const time = Date.parse(deadline)-Date.parse(new Date()) ;
-       console.log('time',time) ;
+      
        const seconds =  Math.floor((time % (1000 * 60)) / 1000);
        const minute =  Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
        const hours =  Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -63,10 +72,11 @@ class Clock extends Component {
 
     return(    
         <div>  
-        <div className=" card-data"> {this.state.days} days </div>
-        <div className=" card-data"> {this.state.hours} hours   </div>
-        <div className=" card-data">{this.state.minute}  minute  </div>
-        <div className=" card-data"> {this.state.seconds} seconds </div>
+        
+        <div className=" card-data"> {this.leadingzero(this.state.days)} days </div>
+        <div className=" card-data">{this.leadingzero(this.state.hours)} hours   </div>
+        <div className=" card-data">{this.leadingzero(this.state.minute)}  minute  </div>
+        <div className=" card-data"> {this.leadingzero(this.state.seconds)} seconds </div>
         
    </div>
 
